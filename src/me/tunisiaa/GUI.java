69ce0e5f40143c20a8ce15024ed7f36c7f8211f1
TuNisiAa-guy <federico.toluzzo1@gmail.com>
2023-12-20 10:49:52 +0100

    idk

---
+++ src/me/tunisiaa/GUI.java
@@ -0,0 +1,13 @@
+package me.tunisiaa;
+
+import javax.swing.*;
+
+public class GUI extends JFrame {
+    private final int HEIGHT = 25;
+    private final int WIDTH = 25;
+
+    private JPanel[][] chart;
+    public GUI(){
+
+    }
+}
