6bd9f84eee0c0ffe36e467ea3956ccd85a22c446
TuNisiAa-guy <federico.toluzzo1@gmail.com>
2023-12-20 10:21:30 +0100

    initial commit

--- src/PathFinder.java
+++ src/PathFinder.java
@@ -1,13 +1,30 @@
 import java.util.ArrayList;
-import java.util.Vector;
 
 public class PathFinder {
-    private Node[][] chart = new Node[][];
+    private final int chartWidth;
+    private final int chartHeight;
+    private ArrayList<Node> open = new ArrayList<>();
+    private ArrayList<Node> closed = new ArrayList<>();
+    private Node start;
+    private Node finish;
+    private Node[][] chart;
+    public PathFinder(int sizeX, int sizeY, int posX1, int posY1, int posX2, int posY2){
+        start = new Node(null, posX1, posY1);
+        finish = new Node(null, posX2, posY2);
+        chartWidth = sizeX;
+        chartHeight = sizeY;
+        chart = new Node[chartHeight][chartWidth];
+        initializeChart();
+    }
     private Node getNode(int posX, int posY){
-
+        return chart[posY][posX];
     }
     private void initializeChart(){
-
+        for (int i = 0; i < chartHeight; i++) {
+            for (int j = 0; j < chartWidth; j++) {
+                chart[i][j] = new Node(null, j, i);
+            }
+        }
     }
     class Node{
         private int posY;
@@ -20,6 +37,22 @@ public class PathFinder {
 
         public void setParent(Node parent) {
             this.parent = parent;
+        }
+
+        public int getPosY() {
+            return posY;
+        }
+
+        public void setPosY(int posY) {
+            this.posY = posY;
+        }
+
+        public int getPosX() {
+            return posX;
+        }
+
+        public void setPosX(int posX) {
+            this.posX = posX;
         }
 
         public Node(Node parent, int posX, int posY){
@@ -40,14 +73,36 @@ public class PathFinder {
             return neighbors.toArray(new Node[neighbors.size()]);
         }
 
-        private Node[] reconstructPath(){
-            Node currentNode = this;
+        private Node[] reconstructPath(Node n){
             ArrayList<Node> path = new ArrayList<>();
-            path.add(currentNode);
-            while(currentNode.getParent() != null){
-                path.add(currentNode.getParent());
+            path.add(n);
+            while(n.getParent() != null){
+                path.add(n.getParent());
+                n = n.getParent();
             }
             return path.toArray(new Node[path.size()]);
+        }
+        private static int getPathLength(Node[] path){
+            int distance = 0;
+            for(int i = 0; i < path.length - 1; i++){
+                if(path[i].getPosX() == path[i + 1].getPosX() || path[i].getPosY() == path[i + 1].getPosY()){
+                    distance += 10;
+                }else{
+                    distance += 14;
+                }
+            }
+            return distance;
+        }
+        private Node closestNode(){
+            int minDistance = 0;
+            Node closest = null;
+            for(Node node : open){
+                if(minDistance > getPathLength(reconstructPath(node))){
+                    minDistance = getPathLength(reconstructPath(node));
+                    closest = node;
+                }
+            }
+            return closest;
         }
     }
 }
